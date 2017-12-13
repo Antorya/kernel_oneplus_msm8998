84b9316ad08a02a034803615a50884207acc3899
Fatih Ünsever <fatihunseverr@gmail.com>
2017-12-14 00:48:14 +0300

    synaptics_driver_s3320: fix gestures not working
    Signed-off-by: andip71 <andreasp@gmx.de>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1213,9 +1213,9 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		(gesture_buffer[2] == 0x6d) ? Mgestrue :
 		(gesture_buffer[2] == 0x73) ? Sgestrue : UnkownGestrue;
 	}
-
+/*
 	keyCode = UnkownGestrue;
-	/* Get key code based on registered gesture */
+	//Get key code based on registered gesture
 	switch (gesture) {
 	case DouTap:
 		keyCode = KEY_DOUBLE_TAP;
@@ -1250,6 +1250,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 	default:
 		break;
 	}
+*/
 
 	TPD_ERR("detect %s gesture\n", gesture == DouTap ? "(double tap)" :
 			gesture == UpVee ? "(V)" :
