LineageOS/android_kernel_oneplus_msm8998

drivers: misc OnePlus source imports
Change-Id: I7a821fdd2a8a1eb5f5db8e10724757b2796b9efb
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

drivers/perf/perf_event_armv8.c
@@ -584,7 +584,11 @@ static void armv8pmu_idle_update(struct arm_pmu *cpu_pmu)
 	struct perf_event *event;
 	int idx;
 
+#ifdef VENDOR_CONFIG_ONEPLUS
+	if (!cpu_pmu || !(cpu_pmu->hw_events))
+#else
 	if (!cpu_pmu)
+#endif
 		return;
 
 	hw_events = this_cpu_ptr(cpu_pmu->hw_events);

drivers/usb/core/driver.c
@@ -1785,6 +1785,14 @@ static int autosuspend_check(struct usb_device *udev)
 			 * or else their drivers don't support autosuspend
 			 * and so they are permanently active.
 			 */
+
+#ifdef VENDOR_CONFIG_ONEPLUS
+			if (intf == NULL) {
+				dev_err(&udev->dev, "%s intf = NULL\n", __func__);
+				return -EIO;
+			}
+#endif
+
 			if (intf->dev.power.disable_depth)
 				continue;
 			if (atomic_read(&intf->dev.power.usage_count) > 0)

drivers/usb/dwc3/core.c
@@ -1220,6 +1220,11 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (dwc->maximum_speed == USB_SPEED_UNKNOWN)
 		dwc->max_hw_supp_speed = dwc->maximum_speed = USB_SPEED_SUPER;
 
+#ifdef VENDOR_CONFIG_ONEPLUS
+	pr_info("Force USB running as High speed");
+	dwc->max_hw_supp_speed = dwc->maximum_speed = USB_SPEED_HIGH;
+#endif
+
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;
 

Antorya/kernel_oneplus_msm8998

Signed-off-by: Fatih Ünsever <fatihunseverr@gmail.com>

drivers/perf/perf_event_armv8.c
@@ -584,7 +584,11 @@ static void armv8pmu_idle_update(struct arm_pmu *cpu_pmu)
 	struct perf_event *event;
 	int idx;
 
+#ifdef CONFIG_VENDOR_ONEPLUS
+	if (!cpu_pmu || !(cpu_pmu->hw_events))
+#else
 	if (!cpu_pmu)
+#endif
 		return;
 
 	hw_events = this_cpu_ptr(cpu_pmu->hw_events);

drivers/usb/core/driver.c
@@ -1807,6 +1807,14 @@ static int autosuspend_check(struct usb_device *udev)
 			 * or else their drivers don't support autosuspend
 			 * and so they are permanently active.
 			 */
+
+#ifdef CONFIG_VENDOR_ONEPLUS
+			if (intf == NULL) {
+				dev_err(&udev->dev, "%s intf = NULL\n", __func__);
+				return -EIO;
+			}
+#endif
+
 			if (intf->dev.power.disable_depth)
 				continue;
 			if (atomic_read(&intf->dev.power.usage_count) > 0)

drivers/usb/dwc3/core.c
@@ -1220,6 +1220,11 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (dwc->maximum_speed == USB_SPEED_UNKNOWN)
 		dwc->max_hw_supp_speed = dwc->maximum_speed = USB_SPEED_SUPER;
 
+#ifdef CONFIG_VENDOR_ONEPLUS
+	pr_info("Force USB running as High speed");
+	dwc->max_hw_supp_speed = dwc->maximum_speed = USB_SPEED_HIGH;
+#endif
+
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;
 